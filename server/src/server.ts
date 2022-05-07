92b3b118ec064a5eb47d0b99d96a78a5d22edefa
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-05-07 00:01:01 -0300

    🔧 update server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,4 +9,6 @@ app.use(express.json())
 app.use(routes)
 
 const port = 3333
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server listening on port ${port}`)
+)
