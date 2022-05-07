5358cc3bd428d947475766de23dda61464ca7c80
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-05-07 01:54:43 -0300

    🔧 add editorConfig + prettier

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,17 +1,18 @@
 module.exports = {
+  mode: 'jit',
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
         brand: {
           400: '#996DFF',
-          500: '#8257E5',
-        },
+          500: '#8257E5'
+        }
       },
       borderRadius: {
-        md: '4px',
-      },
-    },
+        md: '4px'
+      }
+    }
   },
-  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
+  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')]
 }
