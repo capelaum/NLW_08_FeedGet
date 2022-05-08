b0a9d4623490b688faab6cad5ab0f1051f7fc83d
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-05-08 19:50:44 -0300

    💄 add more brand colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          100: '#F3F0FF',
+          200: '#E5DBFF',
           400: '#996DFF',
-          500: '#8257E5'
+          500: '#8257E5',
+          600: '#5F3DC4',
+          700: '#331983'
         }
       },
       borderRadius: {
